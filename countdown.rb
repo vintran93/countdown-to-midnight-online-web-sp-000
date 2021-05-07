4557dc56a50ab47bd31cb1eacce38922a5a69757
Vincent Tran <vincentuong@yahoo.com>
2021-05-07 04:13:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-    while 
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      
+      number -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
