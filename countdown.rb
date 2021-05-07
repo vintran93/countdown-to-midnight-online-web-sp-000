7cc5024c66f327a354b7a6a25103670ac0a5c55f
Vincent Tran <vincentuong@yahoo.com>
2021-05-07 03:45:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 #write your code here
 
-def countdown(integer)
+def countdown(number)
   
 end
 
 
-x = 1
-while x < 10
-  puts "#{x} is less than 10"
-  x += 1
+
+while number > 1
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
